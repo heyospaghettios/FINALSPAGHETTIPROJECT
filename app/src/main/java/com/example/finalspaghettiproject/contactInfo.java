017c97d3fa22371af9934a73836f2e651bd9c527
heyospaghettios <55638317+heyospaghettios@users.noreply.github.com>
2020-05-06 02:10:08 -0700

    uhh just basically added buttons to go back to the main page just to be a bit more friendly

--- app/src/main/java/com/example/finalspaghettiproject/contactInfo.java
+++ app/src/main/java/com/example/finalspaghettiproject/contactInfo.java
@@ -1,6 +1,9 @@
 package com.example.finalspaghettiproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +15,15 @@ public class contactInfo extends MainActivity {
         setContentView(R.layout.activity_contact_info);
 
 
+        //just a button to go back to the main menu or activity
+
+        Button button = findViewById(R.id.Contactbtn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(contactInfo.this, MainActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/finalspaghettiproject/instruct.java
+++ app/src/main/java/com/example/finalspaghettiproject/instruct.java
@@ -1,7 +1,9 @@
 package com.example.finalspaghettiproject;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
 
 
 public class instruct extends MainActivity {
@@ -10,6 +12,16 @@ public class instruct extends MainActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.instruct);
+
+        //just a button to go back to the main menu or activity
+        Button button = findViewById(R.id.buttonInfo);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(instruct.this,MainActivity.class));
+            }
+        });
 
 
     }
